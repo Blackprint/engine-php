c7917cb6a701c5bff283f24d646c952e9333ecfc
StefansArya <stefansarya1@gmail.com>
2021-10-30 00:07:03 +0700

    Improve Types with Enum

--- src/Types.php
+++ src/Types.php
@@ -1,31 +1,20 @@
 <?php
-namespace Blackprint\Types;
+namespace Blackprint;
 
-const Functions = '>?><0';
-function Functions(){}
-
-const Numbers = '>?><1';
-function Numbers(&$val){
-	if(!is_numeric($val))
-		throw new \Exception("Can't validate number");
-
-	$val = $val+0;
+enum Types {
+	case Function;
+	case Number;
+	case Array;
+	case String;
+	case Boolean;
 }
 
-const Arrays = '>?><2';
-function Arrays(&$val){
-	if(is_array($val) === false)
-		$val = [$val];
-
-	$val = $val;
-}
-
-const Strings = '>?><3';
-function Strings(&$val){
-	$val = $val.'';
-}
-
-const Booleans = '>?><4';
-function Boolean(&$val){
-	$val = !!$val;
+function getTypeName(&$type){
+	return match($type){
+		Types::Function => 'Function',
+		Types::Number => 'Number',
+		Types::Array => 'Array',
+		Types::String => 'String',
+		Types::Boolean => 'Boolean',
+	};
 }
