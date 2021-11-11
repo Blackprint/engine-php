d733682df3b7b6b47e7afe4827439c3e9b8ed2e8
StefansArya <stefansarya1@gmail.com>
2021-11-11 16:11:59 +0700

    Add placeholder for Environment feature

---
+++ src/Environment.php
@@ -0,0 +1,18 @@
+<?php
+namespace Blackprint;
+
+class Environment{
+	public static $map = [];
+
+	public static function import($arr){
+
+	}
+
+	public static function set($key, $val){
+
+	}
+
+	public static function delete($key){
+
+	}
+}
